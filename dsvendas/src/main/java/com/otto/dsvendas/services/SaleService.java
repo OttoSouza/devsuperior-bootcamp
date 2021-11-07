7507dc621f72ef6380d27ce5d7708e25d4e490a4
Otto de Souza <otto@pop-os.localdomain>
2021-11-07 16:31:59 -0300

    Pagination

---
+++ dsvendas/src/main/java/com/otto/dsvendas/services/SaleService.java
@@ -0,0 +1,29 @@
+package com.otto.dsvendas.services;
+
+import com.otto.dsvendas.dto.SaleDTO;
+import com.otto.dsvendas.entities.Sale;
+import com.otto.dsvendas.repositories.SaleRepository;
+import com.otto.dsvendas.repositories.SellerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+public class SaleService {
+
+    @Autowired
+    private SaleRepository saleRepository;
+
+    @Autowired
+    private SellerRepository sellerRepository;
+
+    @Transactional(readOnly = true)
+    public Page<SaleDTO> findAll(Pageable pageable) {
+        sellerRepository.findAll();
+        Page<Sale> result = saleRepository.findAll(pageable);
+        return result.map(item -> new SaleDTO(item));
+    }
+}
