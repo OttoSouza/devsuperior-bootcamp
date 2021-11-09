48c312b40f226c1c967da17c109dfc16eb1751ae
OttoSouza <ottos.developer@gmail.com>
2021-11-08 22:10:57 -0300

    DonoutChart integration

--- dev-front/src/components/Barcharts/index.tsx
+++ dev-front/src/components/Barcharts/index.tsx
@@ -1,6 +1,57 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import Chart from "react-apexcharts";
+import { SaleSuccess } from "service/sales";
+import { round } from "utils/format";
+import { BASE_URL } from "utils/request";
+
+type SeriesProps = {
+  name: string;
+  data: number[];
+};
+
+interface BarchartProps {
+  labels: {
+    categories: string[];
+  };
+  series: SeriesProps[];
+}
 
 const Barchart = () => {
+  const [data, setChartData] = useState<BarchartProps>({
+    labels: {
+      categories: [],
+    },
+    series: [
+      {
+        name: "",
+        data: [],
+      },
+    ],
+  });
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/sales/success-by-seller`).then((response) => {
+      const data = response.data as SaleSuccess[];
+      const myLabels = data.map((item) => item.sellerName);
+      const mySeries = data.map((item) =>
+        round((100.0 * item.deals) / item.visited, 1)
+      );
+
+      setChartData({
+        labels: {
+          categories: myLabels,
+        },
+        series: [
+          {
+            name: "% success",
+            data: mySeries,
+          },
+        ],
+      });
+    });
+  }, []);
+
   const options = {
     plotOptions: {
       bar: {
@@ -23,8 +74,8 @@ const Barchart = () => {
 
   return (
     <Chart
-      options={{ ...options, xaxis: mockData.labels }}
-      series={mockData.series}
+      options={{ ...options, xaxis: data.labels }}
+      series={data.series}
       type="bar"
       height="240"
     />
--- dev-front/src/components/DonutChart/index.tsx
+++ dev-front/src/components/DonutChart/index.tsx
@@ -1,44 +1,47 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import Chart from "react-apexcharts";
-import { SaleSum } from "service/sales";
-import { BASE_URL } from "utils/request";
-
-type DonoutChartProps = {
-  labels: string[];
-  series: number[];
-};
-
-const Donoutchart = () => {
-  const [data, setData] = useState<DonoutChartProps>({} as DonoutChartProps);
-
-  useEffect(() => {
-    axios.get(`${BASE_URL}/sales/amount-by-seller`).then((response) => {
-      const data = response.data as SaleSum[];
-      const myLabels = data.map((item) => item.sellerName);
-      const mySeries = data.map((item) => item.sum);
-
-      setData({ labels: myLabels, series: mySeries });
-      console.log(data);
-    });
-  }, []);
-
-  const options = {
-    legend: {
-      show: true,
-    },
-  };
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import Chart from 'react-apexcharts';
+import { SaleSum } from 'service/sales';
+import { BASE_URL } from 'utils/request';
 
 
+type ChartData = {
+    labels: string[]
+    series: number[]
+}
 
-  return (
-    <Chart
-      options={{ ...options, labels: data.labels }}
-      series={data.series}
-      type="donut"
-      height="240"
-    />
-  );
-};
 
-export default Donoutchart;
+const DonutChart = () => {
+    const [chartData, setChartData] = useState<ChartData>({
+        labels: [],
+        series: []
+    })
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/amount-by-seller`)
+            .then((response) => {
+                const data = response.data as SaleSum[]
+                const myLabels = data.map(x => x.sellerName)
+                const mySeries = data.map(x => x.sum)
+
+                setChartData({ labels: myLabels, series: mySeries })
+            })
+    }, [])
+
+    const options = {
+        legend: {
+            show: true
+        }
+    }
+
+    return (
+        <Chart
+            options={{ ...options, labels: chartData.labels }}
+            series={chartData.series}
+            type="donut"
+            height="240"
+        />
+    );
+}
+
+export default DonutChart;
